b9937b866db96b4d1732ad44ecb8c911d0560b98
ant0n <shtanakof@yandex.ru>
2017-04-04 16:29:53 +0700

    puper site

--- quize/urls.py
+++ quize/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$',views.post_list, name='post_list')
+	url(r'^$',views.post_list, name='post_list'),
+	url(r'^user/(?P<pk>[а-яА-ЯёЁa-zA-Z0-9]+)/$', views.user_detail, name='user_detail'),
 ]
--- quize/views.py
+++ quize/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-from .models import User, Kon
+from django.shortcuts import render, get_object_or_404
+from .models import User, Kon, Vid
 
 # Create your views here.
 def post_list(request):
 	konkurs = Kon.objects.all()
-	return render(request, 'quize/post_list.html', {'konkurs':konkurs})
+	user = User.objects.all()
+	return render(request, 'quize/post_list.html', {'konkurs':konkurs, 'user':user})
+
+def user_detail(request, pk):
+	user = get_object_or_404(User, pk=pk)
+	#vid = Vid.objects.filter(user=user)
+	konkurs = Kon.objects.all()
+	return render(request, 'quize/user_detail.html', {'user':user, 'konkurs':konkurs})
